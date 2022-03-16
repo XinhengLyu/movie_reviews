1be042bb9fa1f9cdec9d6f8bc7f3988cd8c6199e
XinhengLyu <2642528L@student.gla.ac.uk>
2022-03-16 18:13:37 +0000

    static

--- movie_reviews/urls.py
+++ movie_reviews/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-    path('rango/', include('rango.urls')),
+    path('movie/', include('rango.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
